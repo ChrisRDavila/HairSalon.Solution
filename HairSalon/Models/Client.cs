bdcaa5c1e175de5398deb02d19c633a446391a7b
ChrisRDavila <crossdavila@gmail.com>
2024-02-02 13:57:10 -0800

    add Stylist Details page that will display specified stylist with properties attached to id passed from actionlink stylist clicked on stylist index page, as well as associated clients.  also set up links to edit, delete stylist and link to home index

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -5,7 +5,7 @@ namespace HairSalon.Models
   public class Client
   {
     public int ClientId { get; set; }
-    public string Name { get; set; }
+    public string CustomerName { get; set; }
     public DateTime Appointments { get; set; }
     public int StylistId { get; set; }
     public Stylist Stylist { get; set; }
